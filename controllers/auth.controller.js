33da8e9486142919afd1a3eab59ba45a02c009fe
Harshit Gulati <harshitgulati6126@gmail.com>
2026-09-08 16:53:50 +0530

    feat: added email verification and resend verfication email feature

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,7 @@ import User from "../models/user.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { sendMail, emailVerificationTemplate } from "../utils/mail.js";
 import { tokenGenerator } from "../utils/tokenGenerator.js";
+import { createHmac } from "crypto";
 
 export const registerUser = asyncHandler(async (req, res) => {
   const { email, userName, password, role } = req.body;
@@ -29,7 +30,7 @@ export const registerUser = asyncHandler(async (req, res) => {
     subject: "Email Verification",
     mailgenContent: emailVerificationTemplate(
       user.userName,
-      `${req.protocol}://${req.get("host")}/api/v1/user/verify-email/${unhashedToken}`,
+      `${req.protocol}://${req.get("host")}/api/v1/auth/verify-email/${unhashedToken}`,
     ),
   }).catch((error) => {
     console.error("Email verification message was not sent:", error);
@@ -75,8 +76,7 @@ export const loginUser = asyncHandler(async (req, res) => {
 });
 
 export const logoutUser = asyncHandler(async (req, res) => {
-  const userId = req.user._id;
-  const updatedUser = await User.findByIdAndUpdate(req.user._id, {
+  await User.findByIdAndUpdate(req.user._id, {
     $set: { refreshToken: "" },
   });
 
@@ -85,4 +85,72 @@ export const logoutUser = asyncHandler(async (req, res) => {
     .clearCookie("refreshToken")
     .status(200)
     .json(new apiResponse(200, null, "Logout successful"));
+});
+
+export const currentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(
+      new apiResponse(200, req.user, "currect user data fetched successfully"),
+    );
+});
+
+export const verifyEmail = asyncHandler(async (req, res) => {
+  const { verificationToken } = req.params;
+  if (!verificationToken)
+    throw new apiError(400, "Email verification token is required");
+
+  const hashedVerificationToken = createHmac("sha256", process.env.JWT_SECRET)
+    .update(verificationToken)
+    .digest("hex");
+
+  const user = await User.findOne({
+    emailVerificationToken: hashedVerificationToken,
+    emailVerificationTokenExpiry: { $gt: Date.now() },
+  });
+
+  if (!user) throw new apiError(400, "token is invalid or expired");
+
+  user.isEmailVerified = true;
+
+  user.emailVerificationToken = undefined;
+  user.emailVerificationTokenExpiry = undefined;
+
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, undefined, "Email verification complete"));
+});
+
+export const resendVerificationMail = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) throw new apiError(404, "User doesn't exist");
+
+  if (user.isEmailVerified)
+    throw new apiError(409, "Email is already verified");
+  const { unhashedToken, hashedToken, tokenExpiry } =
+    await user.generateTemporaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationTokenExpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
+
+  sendMail({
+    email: user.email,
+    subject: "Email Verification",
+    mailgenContent: emailVerificationTemplate(
+      user.userName,
+      `${req.protocol}://${req.get("host")}/api/v1/auth/verify-email/${unhashedToken}`,
+    ),
+  }).catch((error) => {
+    console.error("Email verification message was not sent:", error);
+  });
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, null, "Verification email sent successfully"));
 });
--- routes/user.router.js
+++ routes/user.router.js
@@ -2,6 +2,9 @@ import {
   loginUser,
   registerUser,
   logoutUser,
+  currentUser,
+  verifyEmail,
+  resendVerificationMail,
 } from "../controllers/auth.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import { Router } from "express";
@@ -18,4 +21,9 @@ userRouter.post("/login", loginVlidator, validateRequest, loginUser);
 
 userRouter.post("/logout", authMiddleware, logoutUser);
 
+userRouter.get("/getCurrentUser", authMiddleware, currentUser);
+
+userRouter.get("/verify-email/:verificationToken", verifyEmail);
+
+userRouter.post("/resend-email-verification", resendVerificationMail);
 export default userRouter;
